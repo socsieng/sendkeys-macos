11700f3ea52e438a3c36de3e3ec29b3e3241542d
Soc Sieng <socsieng@gmail.com>
2020-09-17 19:04:06 -0700

    refactor: move constants and expressions into type function

--- src/script.template.js
+++ src/script.template.js
@@ -1,52 +1,6 @@
 const app = Application(/** application-name **/);
 const sysevents = Application('System Events');
 
-const KEYS = {
-  'f1': 122,
-  'f2': 120,
-  'f3': 99,
-  'f4': 118,
-  'f5': 96,
-  'f6': 97,
-  'f7': 98,
-  'f8': 100,
-  'f9': 101,
-  'f10': 109,
-  'f11': 103,
-  'f12': 111,
-  'esc': 53,
-  'return': 36,
-  'enter': 76,
-  'delete': 51,
-  'space': 49,
-  'tab': 48,
-  'up': 126,
-  'down': 125,
-  'left': 123,
-  'right': 124,
-  'home': 115,
-  'end': 119,
-  'pgup': 116,
-  'pgdown': 121,
-};
-const MODIFIERS = {
-  '⌘': 'command down',
-  '^': 'control down',
-  '⌥': 'option down',
-  '⇧': 'shift down',
-  'command': 'command down',
-  'control': 'control down',
-  'option': 'option down',
-  'shift': 'shift down',
-};
-
-// matches <p:0.1>
-const pauseExpression = /^\<p:([\d.]+)\>/i;
-// matches <c:down>, <c:up:command>
-const charExpression = /^\<c:(.|[\w]+)(:([,\w⌘^⌥⇧]+))?\>/i;
-// matches <\>
-const continuationExpression = /^\<\\\>/;
-
 /** type-function **/
 
 function run(input, parameters) {
@@ -54,7 +8,7 @@ function run(input, parameters) {
 
   delay(/** initial-delay **/);
 
-/** type-commands **/
+  /** type-commands **/
 
   return input;
 }
